21664b51be4af60d24cf7815d20c8705a61499c7
Hrithikpol <88582499+Hrithikpol@users.noreply.github.com>
2024-03-10 17:13:52 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE product_category (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE product (product
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  sku VARCHAR(255) NOT NULL,
+  category_id INT NOT NULL,
+  inventory_id INT,
+  price DECIMAL(10,2) NOT NULL,
+  discount_id INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP DEFAULT NULL,
+  FOREIGN KEY (category_id) REFERENCES product_category(id)
+);
